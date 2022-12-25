b8e8c35946bdb76feeeb778380c72598d4ef1423
LambFerret <dignrjrl12@gmail.com>
2022-12-25 17:47:45 +0900

    init

---
+++ src/main/java/com/lambferret/playhttpground/PlayhttpgroundApplication.java
@@ -0,0 +1,13 @@
+package com.lambferret.playhttpground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayhttpgroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayhttpgroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lambferret/playhttpground/PlayhttpgroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambferret.playhttpground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayhttpgroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
